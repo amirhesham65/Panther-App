263ddbd306e198d077f81190290943aaeaea475d
amirhesham65 <amirhesham65@gmail.com>
2019-10-13 21:52:18 +0200

    Reformating and cleaning code

--- lib/components/task_card.dart
+++ lib/components/task_card.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class TaskCard extends StatefulWidget {
-  String projectName;
-  String taskTitle;
-  String taskDescription;
+  // Accepting data
+  final String projectName;
+  final String taskTitle;
+  final String taskDescription;
 
   TaskCard({this.projectName, this.taskTitle, this.taskDescription});
 
@@ -12,7 +13,6 @@ class TaskCard extends StatefulWidget {
 }
 
 class _TaskCardState extends State<TaskCard> {
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -24,37 +24,66 @@ class _TaskCardState extends State<TaskCard> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               SizedBox(height: 10.0),
-              Text(widget.projectName, style: TextStyle(color: Theme.of(context).primaryTextTheme.caption.color)),
+              Text(
+                widget.projectName,
+                style: TextStyle(
+                    color: Theme.of(context).primaryTextTheme.caption.color),
+              ),
               SizedBox(height: 10.0),
-              Text(widget.taskTitle, style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold), ),
+              Text(
+                widget.taskTitle,
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
               SizedBox(height: 10.0),
-              Text(widget.taskDescription, style: TextStyle(fontSize: 12.0, color: Theme.of(context).primaryTextTheme.caption.color)),
+              Text(
+                widget.taskDescription,
+                style: TextStyle(
+                    fontSize: 12.0,
+                    color: Theme.of(context).primaryTextTheme.caption.color),
+              ),
               // Text('A card is a sheet of Material used to represent some related information, for example an album.', style: TextStyle(fontSize: 12.0, height: 1.5, color: Theme.of(context).primaryTextTheme.caption.color)),
               Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 15.0, horizontal: 0.0),
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 0.0),
                 child: Transform.scale(
                   scale: 1,
                   child: Container(
-                    
                     width: 350.0,
                     height: 40.0,
                     child: Stack(
                       fit: StackFit.expand,
                       children: <Widget>[
                         Positioned(
-                          child: CircleAvatar(child: Text('A', style: TextStyle(color: Theme.of(context).canvasColor),),  backgroundColor: Colors.greenAccent[200]),
+                          child: CircleAvatar(
+                            child: Text(
+                              'A',
+                              style: TextStyle(
+                                color: Theme.of(context).canvasColor,
+                              ),
+                            ),
+                            backgroundColor: Colors.greenAccent[200],
+                          ),
                           left: 0.0,
                         ),
                         Positioned(
                           child: CircleAvatar(
-                              child: Text('K', style: TextStyle(color: Theme.of(context).canvasColor),),
-                              backgroundColor: Colors.blueAccent[200]),
+                            child: Text(
+                              'K',
+                              style: TextStyle(
+                                color: Theme.of(context).canvasColor,
+                              ),
+                            ),
+                            backgroundColor: Colors.blueAccent[200],
+                          ),
                           left: 30.0,
                         ),
                         Positioned(
                           child: CircleAvatar(
-                            child: Text('H', style: TextStyle(color: Theme.of(context).canvasColor),),
+                            child: Text(
+                              'H',
+                              style: TextStyle(
+                                color: Theme.of(context).canvasColor,
+                              ),
+                            ),
                             backgroundColor: Colors.redAccent[200],
                           ),
                           left: 60.0,
