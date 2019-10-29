82a8c1be426a1c666862766e5aba609346041e0a
amirhesham65 <amirhesham65@gmail.com>
2019-10-29 18:34:01 +0200

    Refactoring the auth service

---
+++ lib/auth.dart
@@ -0,0 +1,41 @@
+import 'package:google_sign_in/google_sign_in.dart';
+
+// Defining the scope of data fetched from Google Authentication
+GoogleSignIn _googleSignIn = GoogleSignIn(scopes: <String>['email']);
+
+class AuthService {
+  // The data
+  GoogleSignInAccount _currentUser;
+
+  void setUser(GoogleSignInAccount fetchedAccout) {
+    _currentUser = fetchedAccout;
+  }
+
+  // Initializing the user authentication listener 
+  void initGoogleAuthListner(Function callBack) {
+    // Listening to any SignIn/SignOut changes
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
+      _currentUser = account;
+      if (_currentUser != null) {
+         callBack(_currentUser);
+        }
+      // SigningIn with Google silently
+      _googleSignIn.signInSilently();
+    });
+  }
+
+  // Handling SigningIn with Google
+  Future<void> handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  // Handling SigningOut from Google
+  Future<void> handleSignOut() async {
+    await _googleSignIn.disconnect();
+    print('Disconnected!');
+  }
+}
--- lib/views/WelcomeView.dart
+++ lib/views/WelcomeView.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:panther_app/auth.dart';
 import 'package:panther_app/components/GoogleSignInButton.dart';
 
-// Defining the scope of data fetched from Google Authentication
-GoogleSignIn _googleSignIn = GoogleSignIn(scopes: <String>['email']);
+AuthService auth = AuthService();
 
 // The Welcome/SignIn View. Has the introduction of the app and authentication
 class WelcomeView extends StatefulWidget {
@@ -12,38 +11,14 @@ class WelcomeView extends StatefulWidget {
 }
 
 class _WelcomeViewState extends State<WelcomeView> {
-  GoogleSignInAccount _currentUser; // Saving the user in the state
 
   @override
   void initState() {
     super.initState();
-    // Listening to any SignIn/SignOut changes
-    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
-      setState(() {
-        // Update the user instance in the state
-        _currentUser = account;
-        // Check if user instance was fetched
-        if (_currentUser != null) {
-          print(_currentUser.displayName);
-        }
-      });
-      // SigningIn with Google silently
-      _googleSignIn.signInSilently();
+    // Setup the Auth listener
+    auth.initGoogleAuthListner((user) {
+      print('Hello, ${user.displayName}!');
     });
-  }
-
-  // Handling SigningIn with Google
-  Future<void> _handleSignIn() async {
-    try {
-      await _googleSignIn.signIn();
-    } catch (error) {
-      print(error);
-    }
-  }
-
-  // Handling SigningOut from Google
-  Future<void> _handleSignOut() async {
-    _googleSignIn.disconnect();
   }
 
   @override
@@ -55,26 +30,24 @@ class _WelcomeViewState extends State<WelcomeView> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(
-                'Welcome to Panther',
-                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w100),
-              ),
+              Text('Welcome to Panther', style: TextStyle(fontSize: 14.0, color: Colors.grey),),
               Text(
                 'Productivity done better',
                 style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
               ),
               SizedBox(height: 30.0),
-              GoogleSignInButton(onPressed: _handleSignIn),
+              GoogleSignInButton(onPressed: auth.handleSignIn),
               SizedBox(height: 30.0),
+              RaisedButton(
+                child: Text('SignOut'),
+                onPressed: auth.handleSignOut
+              ),
               Container(
                 width: 300,
                 child: Text(
                   'By continuing, you agree to Panther\'s Terms of Service, Privacy Policy.',
                   textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                    fontWeight: FontWeight.w100,
-                  ),
+                  style: TextStyle(color: Colors.grey[500]),
                 ),
               ),
             ],
